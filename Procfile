ddd00c613381c3683110d23f0d5931e7712d5c80
Vishal Chaudhary <vishal.shubham@gmail.com>
2018-04-18 00:43:39 -0400

    First Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/news-ticker-api-1.0.jar
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  private final String USER_AGENT = "Mozilla/5.0";
+
+  @Autowired
+  private DataSource dataSource;
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Main.class, args);
+    System.out.println(args.toString());
+  }
+
+  @RequestMapping("/")
+  String index() {
+    return "index";
+  }
+
+  @RequestMapping("/stockData")
+  String getStockData(Map<String, Object> model) {
+    try {
+
+      URL obj = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=60min&apikey=F0FYC65W5BBQBQI4");
+  
+      HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+  
+      // optional default is GET
+      con.setRequestMethod("GET");
+  
+      //add request header
+      con.setRequestProperty("User-Agent", USER_AGENT);
+  
+      int responseCode = con.getResponseCode();
+      System.out.println("\nSending 'GET' request to URL : " + "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=60min&apikey=F0FYC65W5BBQBQI4");
+      System.out.println("Response Code : " + responseCode);
+  
+      BufferedReader in = new BufferedReader(
+              new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      StringBuffer response = new StringBuffer();
+  
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+      in.close();
+  
+      //print result
+      System.out.println(response.toString());
+      return response.toString();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return "Sorry no data available due to internal error";
+  }
+
+
+  @RequestMapping("/hello")
+  String dbb(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Vishal read from DB: " + rs.getTimestamp("tick"));
+      }
+
+      model.put("records", output);
+      return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @RequestMapping("/db")
+  String db(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Read from DB: " + rs.getTimestamp("tick"));
+      }
+
+      model.put("records", output);
+      return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @Bean
+  public DataSource dataSource() throws SQLException {
+    if (dbUrl == null || dbUrl.isEmpty()) {
+      return new HikariDataSource();
+    } else {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+    }
+  }
+
+}
