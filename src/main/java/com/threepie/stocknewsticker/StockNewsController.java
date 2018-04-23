266578d72994739dae84f3930ae65becc34ef737
Vishal Chaudhary <vishal.shubham@gmail.com>
2018-04-23 19:56:47 -0400

    Added stockData Class

--- src/main/java/com/threepie/stocknewsticker/StockNewsController.java
+++ src/main/java/com/threepie/stocknewsticker/StockNewsController.java
@@ -1,6 +1,15 @@
 package com.threepie.stocknewsticker;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -8,6 +17,19 @@ public class StockNewsController {
 
 	@RequestMapping("/hell")
     public String index() {
-        return "Greetings from Spring Boot!";
+		
+		final String uri = "https://newsapi.org/v2/sources?apiKey=bebc2e5b6fe64fdd94aadbb64fd02bfa";
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+	     
+	    ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+	     
+	    return result.toString();
+	    
+        // return "Greetings from Spring Boot!";
     }
 }
