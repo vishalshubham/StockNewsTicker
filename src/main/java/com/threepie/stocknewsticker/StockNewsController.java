f025e8b8893ff589c85512eb58363ef59852e5eb
Vishal Chaudhary <vishal.shubham@gmail.com>
2018-04-23 22:07:41 -0400

    Added stockData Class

--- src/main/java/com/threepie/stocknewsticker/StockNewsController.java
+++ src/main/java/com/threepie/stocknewsticker/StockNewsController.java
@@ -2,6 +2,7 @@ package com.threepie.stocknewsticker;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -12,7 +13,7 @@ public class StockNewsController {
 		handler = new StockNewsHandler();
 	}
 
-	@RequestMapping("/stockData")
+	@RequestMapping(value = "/stockdata", method = RequestMethod.GET, produces = "application/json")
     public String getStockData(
     		@RequestParam("symbol") String symbol,
     		@RequestParam("from") String from,
